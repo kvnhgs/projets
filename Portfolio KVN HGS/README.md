b48fe3f73b5ad9d9183197218f0b3a5fa82a0241
Kévin H <kevin@kvn-hgs.com>
2024-01-05 14:01:59 +0100

    KVN HGS PORTFOLIO

---
+++ Portfolio KVN HGS/README.md
@@ -0,0 +1,11 @@
+Bonjour Louis, 
+
+Tu trouveras ci-joint le projet protfolio KVN HGS en HTML CSS JAVASCRIPT.
+
+Voici les éléments à résoudre :
+- Couleur onglet actif profil
+- Formulaire de contact inactif
+
+Bonne réception.
+
+Cordialement, Kévin HEUGAS.
---
+++ Portfolio KVN HGS/styles/script.js
@@ -0,0 +1,43 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+  
+    // Cacher tous les contenus des onglets
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Enlever la classe 'active' de tous les onglets
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+    }
+  
+    // Afficher le contenu de l'onglet actuel et ajouter la classe 'active'
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    // Ajouter des écouteurs d'événements pour chaque onglet
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+      tablinks[i].addEventListener("click", function(event) {
+        openTab(event, this.getAttribute("data-tab"));
+      });
+    }
+  
+    // Ouvrir l'onglet par défaut
+    var defaultTab = document.getElementById("defaultOpen");
+    if (defaultTab) {
+      defaultTab.click();
+    }
+  });
+  
+  document.querySelectorAll('.dynamicButton').forEach(function(button) {
+    button.addEventListener('click', function() {
+        // La ligne suivante déclenche une boîte d'alerte, nous allons donc la commenter.
+        // alert('Bouton ' + this.textContent + ' cliqué!');
+    });
+});
+
